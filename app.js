3158e739e2009a64e71ef6a952e47baf6a5001a7
JokerJF <djokerd09@gmail.com>
2024-10-14 20:17:14 +0500

    test2

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config(); // Импорт dotenv
+const fs = require('fs'); // Импортируем модуль fs
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Используйте PORT из .env, если он не определен, используйте 3000
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
 let players = [];
+
+// Функция для загрузки игроков из файла
+function loadPlayers() {
+    try {
+        const data = fs.readFileSync('save.json', 'utf8');
+        data.length != 0 ? players = JSON.parse(data) : []
+        // players = JSON.parse(data);
+        // console.log(data);
+    } catch (err) {
+        console.error('Error reading save.json:', err);
+    }
+}
+
+// Функция для сохранения игроков в файл
+function savePlayers(pls) {
+    try {
+        fs.writeFileSync('save.json', JSON.stringify(pls, null, 2));
+    } catch (err) {
+        console.error('Error writing to save.json:', err);
+    }
+}
+
 function getTopPlayers(players) {
     players.sort((a, b) => b.coins - a.coins);
     return players.slice(0, 20);
 }
+
+// Загрузить игроков при старте сервера
+loadPlayers();
 
 app.get('/test', (req, res) => {
     res.end();
@@ -20,11 +46,13 @@ app.get('/test', (req, res) => {
 
 app.get('/list', (req, res) => {
     const topPlayers = getTopPlayers(players);
+    savePlayers(topPlayers); // Сохраняем игроков после добавления нового
     res.json(topPlayers);
 });
 
 app.post('/list', (req, res) => {
     players.push(req.body);
+    savePlayers(players); // Сохраняем игроков после добавления нового
     res.end();
 });
 
